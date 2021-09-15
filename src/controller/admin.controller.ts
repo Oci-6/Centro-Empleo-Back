0ac3135ef4d359d7c7f425b36be7c2e14f25ef88
Facu <51865917+camilillo15@users.noreply.github.com>
2021-09-15 18:15:56 -0300

    algunas cosas de admin y novedad

---
+++ src/controller/admin.controller.ts
@@ -0,0 +1,24 @@
+/*
+
+import {Request,  Response} from 'express';
+import { getRepository } from 'typeorm';
+import { Admin } from '../models/Admin';
+import * as helperAdmin from '../helpers/admin.helper';
+
+/* ----- País Controller ----- 
+
+export const getPaises = async (req: Request, res: Response): Promise<Response> => {
+    return res.status(200).json(await helperPais.getAll());
+}
+
+export const getPais = async (req: Request, res: Response): Promise<Response> => {
+    if(!req.params.id) return res.status(400).json({message: 'No se ingreso id'});
+
+    let pais = await helperPais.get(req.params.id);
+
+    if(!pais) return res.status(400).json({message: 'No existe tal país'});
+
+    return res.status(200).json(pais);
+}
+
+*/
